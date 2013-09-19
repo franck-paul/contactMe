50fd7752381346b9a4f82c44d9e739616637f076
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:30:16 +0200

    various (next release will be 1.4)

--- _admin.php
+++ _admin.php
@@ -1,14 +1,14 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of contactMe, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) Olivier Meunier and contributors
 #
-# Copyright (c) 2003-2011 Olivier Meunier and dcTeam
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
 # -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
 $_menu['Plugins']->addItem(__('Contact me'),'plugin.php?p=contactMe','index.php?pf=contactMe/icon.png',
--- _widgets.php
+++ _widgets.php
@@ -1,14 +1,14 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of contactMe, a plugin for Dotclear 2.
 #
-# This file is part of Dotclear 2.
+# Copyright (c) Olivier Meunier and contributors
 #
-# Copyright (c) 2003-2008 Olivier Meunier and contributors
 # Licensed under the GPL version 2.0 license.
-# See LICENSE file or
+# A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-#
 # -- END LICENSE BLOCK ------------------------------------
+
 if (!defined('DC_RC_PATH')) { return; }
 
 $GLOBALS['core']->addBehavior('initWidgets',array('contactMeWidgets','initWidgets'));
