3f260ab22c4b5a43bdc30f073c0a1ee3f234b430
franck <carnet.franck.paul@gmail.com>
2021-03-06 09:52:15 +0100

    Add publicContactMeFormBeforeContent and publicContactMeFormAfterContent behaviors, closes #4

--- _define.php
+++ _define.php
@@ -10,14 +10,15 @@
  * @copyright Olivier Meunier
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "ContactMe",                              // Name
-    "Add a simple contact form on your blog", // Description
-    "Olivier Meunier and contributors",       // Author
-    '1.11.1',                                 // Version
+    'ContactMe',                              // Name
+    'Add a simple contact form on your blog', // Description
+    'Olivier Meunier and contributors',       // Author
+    '1.12',                                   // Version
     [
         'requires'    => [['core', '2.16']],                         // Dependencies
         'permissions' => 'admin',                                    // Permissions
