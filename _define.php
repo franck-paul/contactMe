3c33a0bccd1d8a1af8d8ae8a1670610ba4bbccf1
franck <carnet.franck.paul@gmail.com>
2024-11-12 13:08:34 +0100

    New release 5.6

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'ContactMe',
     'Add a simple contact form on your blog',
     'Olivier Meunier and contributors',
-    '5.5',
+    '5.6',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
