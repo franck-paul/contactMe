5ec0f925d16f8a6f90630bf409dd2b84709ffec2
franck <contact@open-time.net>
2026-07-28 10:03:14 +0200

    To soon for that!

--- src/FrontendWidgets.php
+++ src/FrontendWidgets.php
@@ -32,8 +32,8 @@ class FrontendWidgets
             return '';
         }
 
-        if (($w->homeonly === 1 && !App::url()->isHome(App::url()->getType()))
-            || ($w->homeonly === 2 && App::url()->isHome(App::url()->getType()))
+        if (($w->homeonly == 1 && !App::url()->isHome(App::url()->getType()))
+            || ($w->homeonly == 2 && App::url()->isHome(App::url()->getType()))
         ) {
             return '';
         }
